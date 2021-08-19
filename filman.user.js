00b4945c660f72aa3d859c120738a029aff1c576
grachel <grachel@softsystem.pl>
2021-08-19 12:50:13 +0200

    v11 revert

--- filman.user.js
+++ filman.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filman.cc
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      0.11
 // @description  Filman script
 // @author       You
 // @match        https://filman.cc/*
