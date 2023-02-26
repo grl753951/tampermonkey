ffef204c402b7d71d28036e88e42354f82d342af
Grzegorz Rachel <grachel17@gmail.com>
2023-02-26 14:40:53 +0100

    Update facebook.user.js

--- facebook.user.js
+++ facebook.user.js
@@ -1,20 +1,16 @@
 // ==UserScript==
 // @name         Facebook
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://m.facebook.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=facebook.com
-// @grant        none
-// @run-at       document-start
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
-    document.addEventListener("DOMContentLoaded", function() {
-    console.log('x')
-          document.getElementById('root').style.display = 'none'
-    });
+    GM_addStyle("#m_news_feed_stream { display: none }");
 
 })();
