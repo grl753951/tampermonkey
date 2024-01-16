cd3123478546a78d3686af639a51d6af5f93781b
Grzegorz Rachel <grachel17@gmail.com>
2024-01-16 10:18:56 +0100

    Update filman.user.js

--- filman.user.js
+++ filman.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filman.cc
 // @namespace    http://tampermonkey.net/
-// @version      0.14
+// @version      0.15
 // @description  Filman script
 // @author       You
 // @match        https://filman.cc/*
@@ -55,13 +55,53 @@
                         if (!item.textContent.includes(selectedVersion) ||
                             (selectedVersion === 'Lektor' && item.textContent.includes('Lektor_IVO')) ||
                             (selectedVersion === 'Napisy' && item.textContent.includes('Napisy_Tansl'))
-                        ) {
+                           ) {
                             item.setAttribute("style", "display: none");
                         }
                     });
                 }
             });
         });
+    }
+
+    function sendToKodi(streamUrl) {
+        if (navigator.share) {
+            navigator.share({
+                title: 'Stream URL',
+                text: 'Watch a video!',
+                url: streamUrl,
+            })
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.log('Error sharing', error));
+        } else {
+            console.log('Share not supported on this browser, do it the old way.');
+        }
+
+        //         try {
+        //             const body = {
+        //                 jsonrpc: '2.0',
+        //                 method: 'Player.Open',
+        //                 id: 0,
+        //                 params: {
+        //                     item: {
+        //                         file: 'plugin://plugin.video.sendtokodi/?' + streamUrl
+        //                     }
+        //                 }
+        //             };
+
+        //             const Http = new XMLHttpRequest();
+        //             const url='http://192.168.1.111:8080/jsonrpc';
+        //             Http.open("POST", url, true);
+        //             Http.withCredentials = true;
+        //             Http.setRequestHeader('Accept', 'application/json');
+        //             Http.setRequestHeader('Content-Type', 'application/json');
+        //             Http.setRequestHeader('Authorization', 'Basic ' + btoa('kodi:kodi'));
+        //             Http.send(JSON.stringify(body));
+        //         }
+        //         catch (error) {
+        //             console.log(error)
+        //             alert(error.message);
+        //         }
     }
 
     function extractLinkData() {
@@ -71,8 +111,14 @@
             item.setAttribute("style", "cursor: pointer");
 
             item.addEventListener('click', function () {
-                const url = JSON.parse(atob(item.getAttribute('data-iframe'))).src;
+                let url;
+                if(item.getAttribute('data-mp4') == null) {
+                    url = JSON.parse(atob(item.getAttribute('data-iframe'))).src;
+                } else {
+                    url = atob(item.getAttribute('href'));
+                }
                 window.open(url);
+                sendToKodi(url);
             }, false);
         });
     }
